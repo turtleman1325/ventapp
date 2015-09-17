91b4407efd2d3d1a2953dd1fac4841867a565493
Art <art19ad4@gmail.com>
2015-09-17 09:20:24 -0700

    Finish static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render text: "hello, venters!"
+    render text: "hello, venters people!"
   end
 end
